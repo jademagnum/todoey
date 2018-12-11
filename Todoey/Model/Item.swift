10310c9507c882967fe80b46e88e045051796db7
Jade Thomason <jadethomason@ElMagniiorChode.hsd1.ut.comcast.net>
2018-12-11 16:55:17 -0700

    first commit

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jade Thomason on 12/11/18.
+//  Copyright © 2018 Jade Thomason. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var name: String = ""
+    var checked: Bool = false
+}
---
+++ Todoey/View Controller/TodoTableViewController.swift
@@ -0,0 +1,172 @@
+//
+//  TodoTableViewController.swift
+//  Todoey
+//
+//  Created by Jade Thomason on 12/11/18.
+//  Copyright © 2018 Jade Thomason. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadItems()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        
+        cell.accessoryType = item.checked ? .checkmark : .none
+        
+        //        The Code above replaces this code below.
+        //        if item.checked == true {
+        //            cell.accessoryType = .checkmark
+        //        } else {
+        //            cell.accessoryType = .none
+        //        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //  print(itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].checked = !itemArray[indexPath.row].checked
+        
+        // The Code above replaces this code below.
+        //        if itemArray[indexPath.row].checked == false {
+        //            itemArray[indexPath.row].checked = true
+        //        } else {
+        //            itemArray[indexPath.row].checked = false
+        //        }
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once user clickes the add item button on our UIAlert
+            let newItem = Item()
+            
+            newItem.name = textField.text ?? ""
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        guard let url = self.dataFilePath else { return }
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: url)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        guard let url = dataFilePath else { return }
+        guard let data = try? Data(contentsOf: url) else { return }
+        let decoder = PropertyListDecoder()
+        
+        do {
+            itemArray = try decoder.decode([Item].self, from: data)
+        } catch {
+            print("Error decoding, \(error)")
+        }
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
